188f464a8b4c1e77ee49661e3e28d4be04510bf0
moeendehqan <moeen.dehqan@gmail.com>
2024-03-15 21:42:33 +0330

    .

--- Function.py
+++ Function.py
@@ -3,28 +3,58 @@ import requests
 import json
 import datetime
 from persiantools.jdatetime import JalaliDate
-client = MongoClient()
+client = MongoClient('mongodb://root:YgXs5qROdTNGqW13EADso3VM@sinai.liara.cloud:30651/my-app?authSource=admin')
 
 db = client['bot']
+pacing = {
+    30:{
+        20 : 60000,
+        30 : 75000,
+        50 : 95000,
+        80 : 135000,
+        130 : 195000,
+        },
+    60:{
+        20 : 95000,
+        30 : 110000,
+        50 : 135000,
+        80 : 170000,
+        130 : 230000,
+    },
+    90:{
+        20 : 135000,
+        30 : 145000,
+        50 : 170000,
+        80 : 205000,
+        130 : 270000,
+    },
+}
+
 
 def Token():
     token = requests.post('http://185.239.0.149:8000/api/admin/token',data={'username':'moeen', 'password':'Moeen....6168'})
     token = json.loads(token.content)
-    print(token)
     token = token['access_token']
     return token
 
 def bits_to_gigabytes(bits):
     bytes = bits * 9.3132257461548E-10
-
     return round(bytes,2)
 
-def Message2db(messageJson):
-    #db['message'].insert_one(messageJson)
-    pass
+
+def gigabytes_to_bits(gigabyte):
+    bytes = gigabyte * (1/9.3132257461548E-10)
+    return round(bytes,0)
+
+
+
+def clean_sub(sub):
+    sub = str(sub).replace('http://185.239.0.149:8000/sub/','')
+    return sub
 
 
 def UserGetUsage(sub):
+    sub = clean_sub(sub)
     response = requests.get(f'http://185.239.0.149:8000/sub/{sub}/info')
     if response.status_code != 200:
         return False
@@ -48,51 +78,84 @@ def UserGetUsage(sub):
 def GetPrice(gig,date):
     GigP = 1000
     DateP = 1800
-    dic = {
-        30:{
-            20 : 60000,
-            30 : 75000,
-            50 : 95000,
-            80 : 135000,
-            130 : 195000,
-            },
-        60:{
-            20 : 95000,
-            30 : 110000,
-            50 : 135000,
-            80 : 170000,
-            130 : 230000,
-        },
-        90:{
-            20 : 135000,
-            30 : 145000,
-            50 : 170000,
-            80 : 205000,
-            130 : 270000,
-        },
-    }
     try:
-        price = dic[date][gig]
-        return price
+        price = int(pacing[date][gig])
+        return {'price':price,'date':date,'gig':gig}
     except:
         try:
-            date = int(int(date)/30)*30
+            date = max(int(int(date)/30)*30,30)
             gig = int(int(gig)/10)*10
-            price = dic[date][gig]
-            return price
+            price = int(pacing[date][gig])
+            return {'price':price,'date':date,'gig':gig}
         except:
             try:
-                price = round(((gig*GigP) + (date*DateP))/5000,0)*5000
-                return price
+                date = int(int(date)/30)*30
+                gig = int(int(gig)/10)*10
+                price = int(round(((gig*GigP) + (date*DateP))/5000,0)*5000)
+                return {'price':price,'date':date,'gig':gig}
             except:
-                return False
+                price = int(pacing[30][20])
+                return {'price':price,'date':30,'gig':20}
 
 
-def GetUser(username):
+def GetUser(sub):
+    sub = clean_sub(sub)
+    username = UserGetUsage(sub)['username']
     token = Token()
-    print(token)
-    user = requests.get(f'http://185.239.0.149:8000/api/user/{username}').content
-    print(user)
+    header = {'accept':'application/json', 'Authorization':f'Bearer {token}'}
+    respone = requests.get(url=f'http://185.239.0.149:8000/api/user/{username}', headers=header)
+    user = json.loads(respone.content)
+    return user
 
 
-GetUser('User-Jfr-106')
+
+def ReNow(sub):
+    sub = clean_sub(sub)
+    userUsage = UserGetUsage(sub)
+    get_price = GetPrice(userUsage['data_limit_gig'],userUsage['timing'])
+    now = datetime.datetime.now()
+    expire_last_day = userUsage['expire_last_day']
+    expire = now + datetime.timedelta(days=get_price['date']+expire_last_day)
+    expire = expire.timestamp()
+    data_limit_avilabel = userUsage['data_limit_gig'] - userUsage['used_traffic_gig']
+    data_limit = gigabytes_to_bits(get_price['gig'] + data_limit_avilabel)
+    username = userUsage['username']
+    token = Token()
+    header = {'accept':'application/json', 'Authorization':f'Bearer {token}', 'Content-Type':'application/json'}
+    data = json.dumps({'expire':expire, 'data_limit':data_limit})
+    response = requests.put(url=f'http://185.239.0.149:8000/api/user/{username}', headers=header, data=data)
+
+def reNew_to_db(username, date, gig, sub, price, chat_id):
+    sub = clean_sub(sub)
+    now = datetime.datetime.now()
+    db['renew'].insert_one({'username':username, 'date':date, 'gig':gig,'sub':sub, 'price':price,'chat_id':chat_id, 'datetime':str(now), 'verify':False})
+
+
+def start_to_db(js):
+    try:
+        db['start'].insert_one(js)
+    except:
+        pass
+
+def sub_by_chat_set(sub, chat_id):
+    sub = clean_sub(sub)
+    now = str(datetime.datetime.now())
+    db['sub_by_chat'].insert_one({'sub':sub,'chat_id':chat_id, 'datetime':now, 'timestump':datetime.datetime.now().timestamp()})
+
+def sub_by_chat_lates_get(chat_id):
+    try:
+        sub = db['sub_by_chat'].find_one({'chat_id':chat_id},sort=[('timestump',-1)])['sub']
+        return sub
+    except:
+        return False
+
+
+def packing():
+    pacNum = 1
+    listE = []
+    for d in pacing:
+        for g in pacing[d]:
+            dic = {'num':pacNum ,'gig':g, 'date':d, 'price':pacing[d][g]}
+            pacNum = pacNum + 1
+            listE.append(dic)
+    return listE
--- bot.py
+++ bot.py
@@ -3,13 +3,57 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import Function
 bot = telebot.TeleBot('7042096088:AAEIU8Kx7AW12gF5-Ra2rnlURTKqsZiFWLo')
+chat_id_admin = '62066305'
+pck = Function.packing()
 bot.delete_webhook()
-user_sub = {}
 
 
-def get_card_info(message):
-    sub = user_sub[message.chat.id]
-    bot.send_message(message.chat.id, f'sub: {sub}')
+def apply_pay_renew_info(message):
+    sub = Function.sub_by_chat_lates_get(message.chat.id)
+    if sub == False:
+        bot.send_message(message.chat.id, 'چنین کاربری یافت نشد')
+    else:
+        get_price = Function.GetPrice(dic['data_limit_gig'],dic['timing'])
+        Function.ReNow(sub)
+        text = f'اشتراک شما تمدید شد.\n'
+        text = text + f'اطلاعات پرداخت شما بعدا توسط پشتیبانی بررسی خواهد'
+        bot.send_message(message.chat.id, text)
+        dic = Function.UserGetUsage(sub)
+        username = dic['username']
+        used_traffic_gig = dic['used_traffic_gig']
+        data_limit_gig = dic['data_limit_gig']
+        used_traffic_rate = dic['used_traffic_rate']
+        expire_jalali = dic['expire_jalali']
+        expire_last_day = dic['expire_last_day']
+        status = dic['status'].replace('active','فعال ✅').replace('disabled','غیرفعال ❌').replace('expired','منقضی ⛔').replace('limited','اتمام حجم 🚨')
+        text = f"سلام {username}!\nوضعیت اشتراک شما:\n"
+        text = text + f" - 🚀 حجم مصرف شده: {used_traffic_gig} گیگابایت\n"
+        text = text + f" - 🚧 حجم باقی مانده: {data_limit_gig} گیگابایت\n"
+        text = text + f" - 📶 درصد مصرف حجم: {used_traffic_rate} %\n"
+        text = text + f" - 📆 تاریخ انقضا: {expire_jalali}\n"
+        text = text + f" - ⏳ مدت اعتبار: {expire_last_day} روز\n"
+        text = text + f" - 🛜 وضعیت: {status}\n"
+        bot.send_message(message.chat.id, text)
+        text_for_admin = f'اطلاعات تمدید:\n'
+        text_for_admin = text_for_admin + f'از کاربر {username}'
+        text_for_admin = text_for_admin + 'مبلغ: '+ str(get_price['price'])+'\n'+'مدت: '+str(get_price['date'])+'\n'+'حجم: '+str(get_price['gig'])+'\n'
+        Function.reNew_to_db(username, get_price['gig'], get_price['date'], sub, get_price['price'], message.chat.id)
+        bot.send_message(chat_id_admin, text_for_admin)
+        bot.forward_message(chat_id=chat_id_admin, from_chat_id=message.chat.id, message_id=message.message_id)
+
+
+def get_pay_renew_info(message):
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    apply = InlineKeyboardButton("تایید ✅", callback_data='apply_renew')
+    Home = InlineKeyboardButton("لغو ❌", callback_data='cancel_renew')
+    keyboard.add(apply, Home)
+    text = 'ایا از اطلاعات ارسال شده مطمعن هستید؟\n'
+    text = text + 'پس از تایید اشتراک شما به صورت خودکار به صورت الحساب تمدید میشود و بعدا اطلاعات پرداخت توسط پشتیبانی بررسی میشود و  در صورت که اطلاعات اشتباه ثبت کرده باشید امکان تمدید از طریق ربات براش شما غیر فعال و فرایند تمدید هم لغو میشود'
+    
+    bot.reply_to(message, text=text, reply_markup=keyboard)
+    
+    
+
 
 
 
@@ -18,22 +62,24 @@ def replay_status(message):
     dic = Function.UserGetUsage(sub)
     if dic == False:
         bot.send_message(message.chat.id, 'اشتراک یافت نشد')
-    user_sub[message.chat.id] = sub
-    username = dic['username']
-    used_traffic_gig = dic['used_traffic_gig']
-    data_limit_gig = dic['data_limit_gig']
-    used_traffic_rate = dic['used_traffic_rate']
-    expire_jalali = dic['expire_jalali']
-    expire_last_day = dic['expire_last_day']
-    status = dic['status'].replace('active','فعال ✅').replace('disabled','غیرفعال ❌').replace('expired','منقضی ⛔').replace('limited','اتمام حجم 🚨')
-    text = f"سلام {username}!\nوضعیت اشتراک شما:\n"
-    text = text + f" - 🚀 حجم مصرف شده: {used_traffic_gig} گیگابایت\n"
-    text = text + f" - 🚧 حجم باقی مانده: {data_limit_gig} گیگابایت\n"
-    text = text + f" - 📶 درصد مصرف حجم: {used_traffic_rate} %\n"
-    text = text + f" - 📆 تاریخ انقضا: {expire_jalali}\n"
-    text = text + f" - ⏳ مدت اعتبار: {expire_last_day} روز\n"
-    text = text + f" - 🛜 وضعیت: {status}\n"
-    bot.send_message(message.chat.id, text)
+    else:
+        Function.sub_by_chat_set(sub, message.chat.id)
+        username = dic['username']
+        used_traffic_gig = dic['used_traffic_gig']
+        data_limit_gig = dic['data_limit_gig']
+        used_traffic_rate = dic['used_traffic_rate']
+        expire_jalali = dic['expire_jalali']
+        expire_last_day = dic['expire_last_day']
+        status = dic['status'].replace('active','فعال ✅').replace('disabled','غیرفعال ❌').replace('expired','منقضی ⛔').replace('limited','اتمام حجم 🚨')
+        text = f"سلام {username}!\nوضعیت اشتراک شما:\n"
+        text = text + f" - 🚀 حجم مصرف شده: {used_traffic_gig} گیگابایت\n"
+        text = text + f" - 🚧 حجم باقی مانده: {data_limit_gig} گیگابایت\n"
+        text = text + f" - 📶 درصد مصرف حجم: {used_traffic_rate} %\n"
+        text = text + f" - 📆 تاریخ انقضا: {expire_jalali}\n"
+        text = text + f" - ⏳ مدت اعتبار: {expire_last_day} روز\n"
+        text = text + f" - 🛜 وضعیت: {status}\n"
+        bot.send_message(message.chat.id, text)
+        handle_start(message)
 
 
 def replay_renew(message):
@@ -41,27 +87,32 @@ def replay_renew(message):
     dic = Function.UserGetUsage(sub)
     if dic == False:
         bot.send_message(message.chat.id, 'اشتراک یافت نشد')
-    user_sub[message.chat.id] = sub
-    username = dic['username']
-    used_traffic_gig = dic['used_traffic_gig']
-    data_limit_gig = dic['data_limit_gig']
-    used_traffic_rate = dic['used_traffic_rate']
-    expire_jalali = dic['expire_jalali']
-    expire_last_day = dic['expire_last_day']
-    price = Function.GetPrice(dic['data_limit_gig'],dic['timing'])
-    cartNumber = '6037697663883889'
-    cartHolder = 'معین دهقان منشادی'
-
-    status = dic['status'].replace('active','فعال ✅').replace('disabled','غیرفعال ❌').replace('expired','منقضی ⛔').replace('limited','اتمام حجم 🚨')
-    text = f"سلام {username}!\nوضعیت اشتراک شما: {status}\n"
-    text = text + f'برای تمدید:\nمبلغ: {price} تومان\nشماره کارت: {cartNumber}\nبه نام {cartHolder}\n واریز و رسید یا اطلاعاتی که حاوی تاریخ و ساعت تراکنش و کارت مبدا و مبلغ باشد را ارسال کنید'
-    msg = bot.send_message(message.chat.id, text)
-    bot.register_next_step_handler(msg, get_card_info)
+    else:
+        Function.sub_by_chat_set(sub, message.chat.id)
+        username = dic['username']
+        GetPrice = Function.GetPrice(dic['data_limit_gig'],dic['timing'])
+        price = GetPrice['price']
+        data_limit_gig = GetPrice['gig']
+        expire_last_day = GetPrice['date']
+        cartNumber = '6037697663883889'
+        cartHolder = 'معین دهقان منشادی'
+        status = dic['status'].replace('active','فعال ✅').replace('disabled','غیرفعال ❌').replace('expired','منقضی ⛔').replace('limited','اتمام حجم 🚨')
+        text = f"سلام {username}!\n"
+        text = text + f"وضعیت  {status}\n\n"
+        text = text + "🚀 افزایش 🚀 \n"
+        text = text + f"⏳ مدت: {expire_last_day} روز\n"
+        text = text + f"📶 حجم: {data_limit_gig} گیگابایت\n"
+        text = text + f'\n\n💰 مبلغ: {price} تومان\n💳 شماره کارت: {cartNumber}\nبه نام {cartHolder}\nواریز و رسید یا اطلاعاتی که حاوی تاریخ و ساعت تراکنش و کارت مبدا و مبلغ باشد را ارسال کنید'
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        Home = InlineKeyboardButton("❌ لغو ❌", callback_data='home')
+        keyboard.add(Home)
+        msg = bot.send_message(message.chat.id, text, reply_markup=keyboard)
+        bot.register_next_step_handler(msg, get_pay_renew_info)
 
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    Function.Message2db(message.json)
+    Function.start_to_db(message.json)
     text = '🤖🔓 خوش آمدید! من ربات "شکن پلاس" هستم. با من در ارتباط باشید و به راحتی از خدمات VPN برای حفظ حریم خصوصی و دسترسی به محتوای محدود شده استفاده کنید. اگر سوالی دارید یا نیاز به راهنمایی دارید، فقط بپرسید. من آماده‌ام که کمک کنم! 🤖🔓'
     keyboard = InlineKeyboardMarkup(row_width=1)
     Buy = InlineKeyboardButton("خرید اشتراک", callback_data='buy')
@@ -69,26 +120,40 @@ def handle_start(message):
     Status = InlineKeyboardButton("وضعیت اشتراک", callback_data='status')
     Help = InlineKeyboardButton("راهنمای استفاده", callback_data='help')
     keyboard.add(Buy, Renew, Status, Help)
-    bot.reply_to(message, text, reply_markup=keyboard)
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
 
 # Handler for callback data from inline keyboard
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
-    if call.data == 'buy':
-        bot.send_message(call.message.chat.id, "خرید")
+    if call.data == 'home':
+        handle_start(call.message)
+    elif call.data == 'buy':
+        bot.send_message(call.message.chat.id, "با توجه به لیست زیر بسته خود را انتخاب کنید")
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        for i in pck:
+            gig = i['gig']
+            date = i['date']
+            price = i['price']
+            name = f'حجم: {gig} گیگ - مدت: {date} روز - قیمت: {price} تومان'
+            pack01 = InlineKeyboardButton(name, callback_data=i['num'])
+            keyboard.add(pack01)
+        bot.send_message(call.message.chat.id, "https://t.me/shekanplus/52", reply_markup=keyboard)
     elif call.data == 'renew':
-        bot.send_message(call.message.chat.id, "تمدید")
         msg = bot.send_message(call.message.chat.id, "توکن یا شناسه اشتراک خود را وارد کنید:")
         bot.register_next_step_handler(msg, replay_renew)
     elif call.data == 'status':
         msg = bot.send_message(call.message.chat.id, "توکن یا شناسه اشتراک خود را وارد کنید:")
+
         bot.register_next_step_handler(msg, replay_status)
     elif call.data == 'help':
-        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard = InlineKeyboardMarkup(row_width=1)
         Android = InlineKeyboardButton("📱 اندروید", callback_data='android')
         ios = InlineKeyboardButton("📱 ای او اس", callback_data='ios')
         windows = InlineKeyboardButton("🖥 ویندوز", callback_data='windows')
-        keyboard.add(Android, ios, windows)
+        Home = InlineKeyboardButton("❌ لغو ❌", callback_data='home')
+
+        keyboard.add(Android, ios, windows, Home)
         bot.send_message(call.message.chat.id, "برای راهنمای اتصال به سرویس ها لطفا سیستم عامل خود را انتخاب کنید", reply_markup=keyboard)
     elif call.data == 'android':
         bot.send_message(call.message.chat.id, "https://t.me/shekanplus/45")
@@ -97,6 +162,19 @@ def handle_callback_query(call):
         bot.send_message(call.message.chat.id, "https://t.me/shekanplus/69")
     elif call.data == 'windows':
         bot.send_message(call.message.chat.id, "https://t.me/shekanplus/70")
+    else:
+        if type(call.data) == int:
+            for i in pck:
+                if int(i['num']) == int(call.data):
+                    text = ''
+                    bot.send_message(call.message.chat.id, i['num'])
+                    break
+        else:
+            print(0)
+            handle_start(call.message)
+
+
+
 
 
 bot.polling()
